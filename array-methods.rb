19ce88280e2bc9735b582c715ebbd55c1b85e7fd
Gulkinator <lgulkus@gmail.com>
2015-08-23 04:06:29 +0000

    moved integer-methods practice.

--- array-methods.rb
+++ array-methods.rb
@@ -1,22 +1,25 @@
 fruits = ['apples','oranges', 'grapes']
 
-fruits.each do |fruit|
-    puts 'I love ' + fruit + '!'
-end
-
+#straight-forward printing of an array.
 puts ' '
 puts 'I love ' + fruits[0] + '!'
 puts 'I love ' + fruits[1] + '!'
 puts 'I love ' + fruits[2] + '!'
 puts ' '
-4.times do
-    puts []
+
+#printing the array using the each method.
+fruits.each do |fruit|
+    puts 'I love ' + fruit + '!'
 end
-puts ' '
+
+#printing the array with puts.
+puts
 puts fruits
 puts
 puts fruits.to_s
 puts
+
+#Using the join method to join array objects.
 puts fruits.join(', ')
 puts
 puts fruits.join(' :)  ')
---
+++ integer-method.rb
@@ -0,0 +1,4 @@
+#using the times method to print a phrase.
+4.times do
+    puts 'Go go gaget times method!'
+end

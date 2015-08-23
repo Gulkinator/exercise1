4133187203fdb646366f6c3c81b4315e4629d7bb
Gulkinator <lgulkus@gmail.com>
2015-08-23 14:59:19 +0000

    added a string-formate-methods exercise.

---
+++ string-format-methods.rb
@@ -0,0 +1,9 @@
+title = 'Table of Contents'.center(50)
+chap1 = 'Getting Started'.ljust(30) + 'page 1'.rjust(20)
+chap2 = 'Numbers'.ljust(30) + 'page 7'.rjust(20)
+chap3 = 'Letters'.ljust(30) + 'page 12'.rjust(20)
+
+puts title
+puts chap1
+puts chap2
+puts chap3
